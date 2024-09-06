b4fdaec07fd709db7cc280f8649c3deda34f742c
Alina <alina.mikhaylova.03@gmail.com>
2024-09-06 16:58:29 +0300

    классный эффект 3дшный на странице постера!!

--- src/modules/poster-card/my-poster-card.js
+++ src/modules/poster-card/my-poster-card.js
@@ -1,24 +1,32 @@
-import cardHTML from "./index.html";
-import "./my-poster-card.scss";
-import myButton from "../../UI/my-button/my-button";
+import cardHTML from './index.html';
+import './my-poster-card.scss';
+import myButton from '../../UI/my-button/my-button';
+import VanillaTilt from 'vanilla-tilt';
 
 function create() {
-  const card = document.createElement("article");
-  card.classList.add("poster-card");
+  const card = document.createElement('article');
+  card.classList.add('poster-card');
   card.innerHTML = cardHTML;
 
-  const sizesCont = card.querySelector(".sizes");
-  const sizeButtons = sizesCont.querySelectorAll("button");
+  const sizesCont = card.querySelector('.sizes');
+  const sizeButtons = sizesCont.querySelectorAll('button');
   sizeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      sizeButtons.forEach((btn) => btn.classList.remove("active"));
-      button.classList.add("active");
+    button.addEventListener('click', () => {
+      sizeButtons.forEach((btn) => btn.classList.remove('active'));
+      button.classList.add('active');
     });
   });
 
+  addTilt(card);
   return card;
 }
 
+function addTilt(card) {
+  VanillaTilt.init(card.querySelector('.card-container'), {
+    max: 8,
+    reverse: true,
+  });
+}
 export default {
   create,
 };
